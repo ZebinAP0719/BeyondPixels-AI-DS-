8c83c0b553889c08c3b85411a59f0858d437b221
Keith-Zidan-25 <keithzidand@gmail.com>
2024-10-24 01:06:45 +0530

    Code Updated again

--- src/scenes/CrimeScene.js
+++ src/scenes/CrimeScene.js
@@ -1,9 +1,20 @@
 import SCENES from '../config/gameConstants.js';
-import Button from '../objects/UI/Button.js'
+import Button from '../objects/UI/Button.js';
+
+var cursors;
+
 class CrimeScene extends Phaser.Scene {
     constructor() {
         super({ key: SCENES.CRIME_SCENE });
         this.evidence = [];
+        cursors = null;
+    }
+
+    preload() {
+        this.load.spritesheet('character', 'assets/images/characters/detective_run.png', {
+            frameWidth: 20, // Width of each frame
+            frameHeight: 39 // Height of each frame
+        });
     }
 
     create() {
@@ -22,8 +33,50 @@ class CrimeScene extends Phaser.Scene {
         for (let i = 0; i <= numPlatforms; i++) {
             floor.create(i * platformWidth, 501, 'museum-floor-platform').setScale(1).refreshBody();
         }
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('character', { start: 0, end: 7 }),
+            frameRate: 15,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('character', { start: 8, end: 15 }),
+            frameRate: 15,
+            repeat: -1
+        });
+    
+        // Create the character sprite
+        this.player = this.physics.add.sprite(100, 200, 'character'); // Make sure sprite key matches
+    
+        // Ensure the sprite has proper scaling and physics properties
+        this.player.setScale(5);
+        this.player.setCollideWorldBounds(true); // Ensure character doesn't go off-screen
+    
+        // Enable arrow key inputs
+        cursors = this.input.keyboard.createCursorKeys();
+
+        this.physics.add.collider(this.player, floor)
+    
         // Initialize UI
         this.createUI();
+    }
+
+    update() {    
+        // Check for left/right movement
+        if (cursors.left.isDown) {
+            this.player.setVelocityX(-160);
+            this.player.anims.play('left', true);
+        } else if (cursors.right.isDown) {
+            this.player.setVelocityX(160);
+            this.player.anims.play('right', true);
+        } else {
+            // If no key is pressed, stop the animation
+            this.player.setVelocityX(0);
+            this.player.anims.stop();
+        }
     }
 
     createUI() {
@@ -37,11 +90,6 @@ class CrimeScene extends Phaser.Scene {
         this.inventoryButton = new Button(this, 700, 550, 'Open Inventory', () => {
             this.scene.start(SCENES.INVENTORY);
         });
-    }
-
-    update() {
-        // Update game logic here
-        this.evidence.forEach(item => item.update());
     }
 }
 
